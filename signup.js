a3350c5ae1c0d7dcb920a033d7080f006ff3e569
chits22 <36458748+chits22@users.noreply.github.com>
2018-02-13 15:53:28 -0600

    Add files via upload

--- signup.js
+++ signup.js
@@ -6,6 +6,10 @@ function validateForm()
         var c=document.forms["Form"]["user_password"].value;
         var d=document.forms["Form"]["user_password2"].value;
 
+    if (c.length<=5){
+        alert("Password too short, please enter one over 5 letters");
+        return false;
+    }
     if (c!=d)
     {
         alert("Passwords don't match :( Please try again!");
@@ -20,3 +24,20 @@ function validateForm()
         }
 
     }
+
+function validateForm2()
+    {
+     
+    var a=document.forms["Forms"]["user_email"].value;
+    var b=document.forms["Forms"]["user_password"].value;
+
+    if (a==null || a=="",b==null || b=="")
+
+        {
+            alert("Please Fill All Required Fields");
+            return false;
+            
+        }
+
+    }
+
