8dcd0683f52ff5aada498aaf91cdccaf64761919
Kenneth Gomez <kenneth@dhcp-10-105-84-46.wireless.northwestern.private>
2018-03-01 13:08:41 -0600

    added chat and finished inbox

---
+++ scripts/home.js
@@ -0,0 +1,18 @@
+var wind = document.getElementById("chat_window");
+
+function chat_pop(){
+  if(toggle){
+    wind.style.opacity = 0;
+    toggle = false;
+  }
+  else{
+    wind.style.opacity = 1
+    toggle = true;
+  }
+}
+
+function chat_show(){
+  wind.style.opacity = 1;
+  toggle = true;
+  console.log("clicked");
+}
---
+++ scripts/uploadProfilePicture.js
@@ -0,0 +1,6 @@
+
+var path_prof = "./images/prof_pic.png";
+
+function setProfilePic(){
+  document.getElementById("profile_picture").src = path_prof;
+}
