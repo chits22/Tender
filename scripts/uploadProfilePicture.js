0f4740353474a8acff9cd070a4a7c621b5057c96
michaelt17 <michaeltamkin2020@u.northwestern.edu>
2018-03-14 16:41:37 -0500

    fix

--- scripts/uploadProfilePicture.js
+++ scripts/uploadProfilePicture.js
@@ -1,6 +1,11 @@
 
 var path_prof = "./images/prof_pic.png";
+var new_prof = "./images/tristan_pic.jpg"
 
 function setProfilePic(){
   document.getElementById("profile_picture").src = path_prof;
+}
+
+function setProfilePicTristan(){
+  document.getElementById("profile_picture").src = new_prof;
 }
